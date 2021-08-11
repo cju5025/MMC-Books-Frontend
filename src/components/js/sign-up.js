a7dc09b5abb77de81ae6d3dfb9acc684d7abb269
Colter Ulrich <cju5025@gmail.com>
2021-08-11 17:54:20 -0600

    adds post request to onSubmit

--- src/components/js/sign-up.js
+++ src/components/js/sign-up.js
@@ -21,24 +21,33 @@ export default class SignUp extends Component {
     handleSubmit = (event) => {
         event.preventDefault()
 
-        if (this.state.email !== '921massive@gmail.com'){
+
+        if (this.state.passwordVerification === this.state.password
+            && this.state.email === '921massive@gmail.com'){
+            fetch('http://localhost:4000/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({
+                    user: {
+                        username: this.state.username,
+                        email: this.state.email,
+                        password: this.state.password
+                    }
+                })
+            })
+            .then(this.setState({ redirect: !this.state.redirect }))
+        } else {
             this.setState({ emailIsMassive: false })
         }
-
-        console.log(this.state)
     }
 
     render () {
         return (
             <div id="signup">
                 <h1>MMC BOOKS SIGN UP</h1>
-                {
-                    this.state.emailIsMassive
-                    ?
-                    null
-                    :
-                    <p>GTFO POSER</p>
-                }
                 <form id="signup-form" onSubmit={this.handleSubmit}>
                     <label>Username:</label>
                     <input onChange={this.handleChange} value={this.state.username} type="text" name="username" placeholder="Username"/>
@@ -53,6 +62,13 @@ export default class SignUp extends Component {
                     <input onChange={this.handleChange} value={this.state.passwordVerification} type="password" name="passwordVerification" placeholder="Verify Password"/>
                     
                     <input type="submit" value="Sign Up" id="sign-up-button" /> 
+                    {
+                        this.state.emailIsMassive
+                        ?
+                        null
+                        :
+                        <p>Either incorrect password or you ain't massive</p>
+                    }
                 </form>
             </div>
         )
