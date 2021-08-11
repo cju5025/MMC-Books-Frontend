7f896b37fed9013802fa3825fd093fa68ae2466d
Colter Ulrich <cju5025@gmail.com>
2021-08-10 20:14:58 -0600

    adds signup page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/js/sign-up';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+import SignUp from './components/js/sign-up';
+
+export default class App extends Component {
+  render () {
+    return (
+      <div id="App">
+        <SignUp />
+      </div>
+    )
+  }
 }
 
-export default App;
