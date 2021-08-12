73e4625c14f75dc64fcd894b3359b9b806290ee9
Colter Ulrich <cju5025@gmail.com>
2021-08-11 18:38:04 -0600

    adds react router

---
+++ src/components/js/sign-in.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+export default class SignIn extends Component {
+
+    render () {
+        return (
+            <div id="sign-in">
+                <h1>Hay</h1>
+            </div>
+        )
+    }
+}
