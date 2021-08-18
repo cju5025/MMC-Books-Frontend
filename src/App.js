df7c6dacabb19b605d62847913cbadab9d00a571
Colter Ulrich <cju5025@gmail.com>
2021-08-18 15:58:21 -0600

    adds overview page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Bandcamp from './components/js/bandcamp';
 import Merch from './components/js/merch';
 import Shows from './components/js/shows';
 import Misc from './components/js/misc';
+import Overview from './components/js/overview';
 
 export default class App extends Component {
   render () {
@@ -26,6 +27,7 @@ export default class App extends Component {
         <Route path="/merch" render={(routerProps) => <Merch {...routerProps} />} />
         <Route path="/shows" render={(routerProps) => <Shows {...routerProps} />} />
         <Route path="/misc" render={(routerProps) => <Misc {...routerProps} />} />
+        <Route path="/overview" render={(routerProps) => <Overview {...routerProps} />} />
         </main>
       </div>
       
