c9c094e5fa1b07d6981345043da82c5162e321fd
Colter Ulrich <cju5025@gmail.com>
2021-08-18 13:00:08 -0600

    integrates in/out functionality to expenses

--- src/components/js/soundcloud.js
+++ src/components/js/soundcloud.js
@@ -8,8 +8,10 @@ export default class Soundcloud extends Component {
     state = {
         type: "Soundcloud",
         amount: 0,
+        inOrOut: '',
         date: "",
-        description: ""
+        description: "",
+        selectInOrOut: false
     }
 
     handleChange = (event) => {
@@ -19,19 +21,37 @@ export default class Soundcloud extends Component {
     handleSubmit = (event) => {
         event.preventDefault();
 
-        fetch('http://localhost:4000/expenses', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({ expense: {
-                type: this.state.type,
-                amount: this.state.amount,
-                date: this.state.date,
-                description: this.state.description
-            }})
-        })
+        if(this.state.inOrOut === 'in'){
+            fetch('http://localhost:4000/expenses', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({ expense: {
+                    type: this.state.type,
+                    amount: parseFloat(this.state.amount),
+                    date: this.state.date,
+                    description: this.state.description
+                }})
+            })
+        } else if (this.state.inOrOut === 'out') {
+                fetch('http://localhost:4000/expenses', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    },
+                    body: JSON.stringify({ expense: {
+                        type: this.state.type,
+                        amount: parseFloat(-Math.abs(this.state.amount)),
+                        date: this.state.date,
+                        description: this.state.description
+                    }})
+                })
+            } else {
+            this.setState({ selectInOrOut: true })
+                }
     }
     
     render () {
@@ -41,9 +61,17 @@ export default class Soundcloud extends Component {
             <main className="expense">
                 <h1>Soundcloud Expenses</h1>
                 <form  onSubmit={this.handleSubmit} id="expense-form">
-                    <label>Amount:</label>
-                    <input onChange={this.handleChange} type="float" name="amount" placeholder="0.00" />
-
+                    <div id="amount-container">
+                        <label>Amount:</label>
+                        <div id="amount-inputs">
+                            <input onChange={this.handleChange} type="float" name="amount" placeholder="0.00" />
+                            <select onChange={this.handleChange} name="inOrOut" id="in-out-select" defaultValue={'DEFAULT'}>
+                                <option value="DEFAULT" disabled>In or Out?</option>
+                                <option value="in">In</option>
+                                <option value="out">Out</option>
+                            </select>
+                        </div>
+                    </div>
                     <label>Date:</label>
                     <input onChange={this.handleChange} type="date" name="date" />
 
@@ -51,6 +79,13 @@ export default class Soundcloud extends Component {
                     <input onChange={this.handleChange} type="text" name="description" placeholder="Sales etc..." />
 
                     <input type="submit" value="Submit" id="submit" />
+                    {
+                        this.state.selectInOrOut
+                        ?
+                        <p>Please Select Whether The Expense Is Incoming Or OutGoing</p>
+                        :
+                        null
+                    }
                 </form>
             </main>
             </>
