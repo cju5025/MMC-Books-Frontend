cfe8e4bc20daf66effdecd3dc1f94487c6e5886e
Colter Ulrich <cju5025@gmail.com>
2021-08-19 20:01:00 -0600

    adds expense file

--- src/components/js/overview.js
+++ src/components/js/overview.js
@@ -1,13 +1,23 @@
 import { Component } from 'react';
 
 import Header from './header';
+import Expense from './expense';
+import '../css/overview.css';
 
 export default class Overview extends Component {
+    
+    componentDidMount = () => {
+        fetch("http://localhost:4000/expenses")
+            .then(response => response.json())
+            .then(console.log)
+    }
+    
     render () {
         return (
             <main id="overview">
                 <Header />
                 <h1>Overview</h1>
+                <Expense />
             </main>
         )
     }
