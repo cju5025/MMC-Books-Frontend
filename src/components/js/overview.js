b323498cab91b90badb1d0e580ae7cdcf2d02dac
Colter Ulrich <cju5025@gmail.com>
2021-08-20 13:45:01 -0600

    renders expenses in overview

--- src/components/js/overview.js
+++ src/components/js/overview.js
@@ -6,10 +6,24 @@ import '../css/overview.css';
 
 export default class Overview extends Component {
     
+    state = {
+        expenses: []
+    }
+
     componentDidMount = () => {
         fetch("http://localhost:4000/expenses")
             .then(response => response.json())
-            .then(console.log)
+            .then(result => result.expenses)
+            .then(expenses => expenses.forEach(expense => {
+                this.setState({ expenses: [...this.state.expenses, expense]})
+            }))
+    }
+
+    showExpenses = () => {
+        return this.state.expenses.map(expense => {
+            console.log(expense)
+            return <Expense amount={expense.amount} date={expense.date} description={expense.description} />
+        })
     }
     
     render () {
@@ -17,7 +31,9 @@ export default class Overview extends Component {
             <main id="overview">
                 <Header />
                 <h1>Overview</h1>
-                <Expense />
+                <section id="expense-section">
+                    {this.showExpenses()}
+                </section>
             </main>
         )
     }
