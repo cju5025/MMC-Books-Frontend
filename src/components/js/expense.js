c86eec193dc788f55129a0b394786e5b48e256bf
Colter Ulrich <cju5025@gmail.com>
2021-08-21 14:59:48 -0600

    styles expense cards

--- src/components/js/expense.js
+++ src/components/js/expense.js
@@ -17,22 +17,25 @@ export default class Expense extends Component {
         const id = this.props.id
         fetch(`http://localhost:4000/expenses/${id}`, { method: "DELETE" })
     }
+
     render () {
         return (
             <div id="expense-card">
-                <button id="delete-button" onClick={this.deleteButtonClicked}>X</button>
-                {
-                    this.state.deleteButtonClicked
-                    ?
-                    <div id="modal">
-                        <p>Are You Sure?</p>
-                        <button onClick={this.deleteExpense}>Yes</button>
-                        <button onClick={this.deleteButtonClicked}>No</button>
-                    </div>
-                    :
-                    null
-                }
-                <p>{this.props.type}</p>
+                <section id="top-section">
+                    <p>{this.props.type}</p>
+                    <button id="delete-button" onClick={this.deleteButtonClicked}>X</button>
+                    {
+                        this.state.deleteButtonClicked
+                        ?
+                        <div id="modal">
+                            <p>Are You Sure?</p>
+                            <button onClick={this.deleteExpense}>Yes</button>
+                            <button onClick={this.deleteButtonClicked}>No</button>
+                        </div>
+                        :
+                        null
+                    }
+                </section>
                 <p>{this.props.description}</p>
                 <p>${this.props.amount}</p>
                 <p>{this.props.date}</p>
